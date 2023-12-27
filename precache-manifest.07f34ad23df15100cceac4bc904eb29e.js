12246b299875982f5f32c124f6b2a12d2c8d9135
Harshit-kumar-1 <harshitkumar1026@gmail.com>
2023-12-27 23:38:21 +0530

    Updates

---
+++ precache-manifest.07f34ad23df15100cceac4bc904eb29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a24731f9a30e7ae024502360c63e22c",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "654d346f5c94aa150df5",
+    "url": "/TodoList/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/TodoList/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "536d1ce6027a3623f9eb",
+    "url": "/TodoList/static/js/3.29f49ed9.chunk.js"
+  },
+  {
+    "revision": "654d346f5c94aa150df5",
+    "url": "/TodoList/static/js/main.163c5678.chunk.js"
+  },
+  {
+    "revision": "c2931a038d24cd9d755e",
+    "url": "/TodoList/static/js/runtime~main.d7f96b2a.js"
+  }
+]);
