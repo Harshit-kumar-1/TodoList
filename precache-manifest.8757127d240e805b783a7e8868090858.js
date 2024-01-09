f86ef9387e95619ea77504e76353b34edaec800b
Harshit-kumar-1 <harshitkumar1026@gmail.com>
2024-01-09 14:51:26 +0530

    Re Deploy

---
+++ precache-manifest.8757127d240e805b783a7e8868090858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ab3e5ab9b419c9b790c8276ad834cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28254621ecc6c3315f0d",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "536d1ce6027a3623f9eb",
+    "url": "/static/js/3.29f49ed9.chunk.js"
+  },
+  {
+    "revision": "28254621ecc6c3315f0d",
+    "url": "/static/js/main.5167caff.chunk.js"
+  },
+  {
+    "revision": "64be39cf1a5b660c9246",
+    "url": "/static/js/runtime~main.37590d49.js"
+  }
+]);
